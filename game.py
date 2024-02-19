c4caddbb2bf7f7827721046b60d158d593ba72d9
igorsiata <147586695+igorsiata@users.noreply.github.com>
2024-02-19 19:43:24 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,116 @@
+
+from pieces import *
+
+
+def position_to_rect(position: [int]) -> (int, int, int, int):
+    return (position % 8) * 100, (position // 8) * 100, 100, 100
+
+
+def mouse_cord() -> int:
+    return 8 * (pygame.mouse.get_pos()[1] // 100) + pygame.mouse.get_pos()[0] // 100
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.size = (800, 800)
+        self.screen = pygame.display.set_mode(self.size)
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.draw_chessboard()
+        self.bluetomove = True
+        self.allpieces = AllPieces()
+        self.ismoving = False
+        self.piecemoving = None
+        self.morecaptures = False
+        self.allpieces.find_all_possible_moves(self.bluetomove)
+
+    def draw_chessboard(self) -> None:
+        for i in range(64):
+            if (i + (i // 8) % 2) % 2 == 0:
+                pygame.draw.rect(self.screen, (232, 218, 161), pygame.Rect((i % 8) * 100, (i // 8) * 100, 100, 100))
+            else:
+                pygame.draw.rect(self.screen, (148, 105, 30), pygame.Rect((i % 8) * 100, (i // 8) * 100, 100, 100))
+
+    def draw_possible_moves(self, pieceindex) -> None:
+        if not self.allpieces.captures:
+            for position in self.allpieces.possiblemoves[pieceindex]:
+                self.screen.blit(pygame.image.load('move.png'), position_to_rect(position))
+        for capture in self.allpieces.captures:
+            if capture[0] == pieceindex:
+                self.screen.blit(pygame.image.load('move.png'), position_to_rect(capture[1]))
+
+    def move(self):
+        mouseposition = mouse_cord()
+        for i in range(len(self.allpieces.allpieces[int(self.bluetomove)])):
+            if mouseposition == self.allpieces.allpieces[int(self.bluetomove)][i].position:
+                self.ismoving = True
+                self.piecemoving = i
+        if self.ismoving:
+            if self.allpieces.captures:
+                for values in self.allpieces.captures:
+                    if mouseposition == values[1]:
+                        self.allpieces.allpieces[int(self.bluetomove)][self.piecemoving].position = mouseposition
+                        self.allpieces.remove_piece(not self.bluetomove, values[2])
+                        self.allpieces.clear_caputres()
+                        self.allpieces.find_possible_moves(self.allpieces.allpieces[int(self.bluetomove)][self.piecemoving], self.bluetomove)
+                        if not self.allpieces.captures:
+                            self.end_move()
+                        else:
+                            self.morecaptures = True
+            elif mouseposition in self.allpieces.possiblemoves[self.piecemoving]:
+                self.allpieces.allpieces[int(self.bluetomove)][self.piecemoving].position = mouseposition
+
+                self.end_move()
+
+    def end_move(self):
+        if self.allpieces.allpieces[int(self.bluetomove)][self.piecemoving].position // 8 == 7 - 7 * int(
+                self.bluetomove):
+            self.allpieces.allpieces[int(self.bluetomove)][self.piecemoving].queen()
+        self.morecaptures = False
+        self.ismoving = False
+        self.piecemoving = None
+        self.bluetomove = not self.bluetomove
+        self.allpieces.find_all_possible_moves(self.bluetomove)
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.move()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and self.morecaptures:
+                self.end_move()
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        self.draw_chessboard()
+
+        if self.ismoving:
+            self.draw_possible_moves(self.piecemoving)
+        for piece in self.allpieces.allpieces[0]:
+            self.screen.blit(piece.image, pygame.Rect(position_to_rect(piece.position)))
+        for piece in self.allpieces.allpieces[1]:
+            self.screen.blit(piece.image, pygame.Rect(position_to_rect(piece.position)))
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        while self.running:
+            if len(self.allpieces.allpieces[0]) == 0:
+                print("Blue wins")
+                self.running = False
+            if len(self.allpieces.allpieces[1]) == 0:
+                print("Red wins")
+                self.running = False
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+            pygame.display.update()
+            self.clock.tick(60)
+        self.on_cleanup()
---
+++ pieces.py
@@ -0,0 +1,93 @@
+import pygame
+
+
+class Piece:
+    def __init__(self, position: int, isblue: bool, isqueen=False):
+        self.position = position
+        self.isblue = isblue
+        self.isqueen = isqueen
+        self.image = pygame.image.load('blue_checker.png') if isblue else pygame.image.load('red_checker.png')
+
+    def queen(self):
+        self.isqueen = True
+        self.image = pygame.image.load('blue_queen.png') if self.isblue else pygame.image.load('red_queen.png')
+
+
+class AllPieces:
+    def __init__(self):
+        self.allpieces = [[Piece(i * 2 + 1 - (i // 4 % 2), False) for i in range(12)],
+                          [Piece(i * 2 + 40 + (i // 4 % 2), True) for i in range(12)]]
+        self.captures = []
+        self.possiblemoves = []
+
+    def get_all_positions(self) -> tuple[list[int], list[int]]:
+        return [piece.position for piece in self.allpieces[0]], [piece.position for piece in self.allpieces[1]]
+
+    def remove_piece(self, bluetomove: bool, index: int) -> None:
+        self.allpieces[int(bluetomove)].pop(index)
+
+    def find_all_possible_moves(self, bluetomove: bool) -> None:
+        self.captures = []
+        self.possiblemoves = []
+        for piece in self.allpieces[int(bluetomove)]:
+            self.find_possible_moves(piece, bluetomove)
+
+    def clear_caputres(self) -> None:
+        self.captures = []
+
+    def find_possible_moves(self, piece: Piece, bluetomove: bool):
+        allpositions = self.get_all_positions()
+        if piece.isqueen:
+            piececaptured = 0
+            possiblemoves = []
+            for i in range(4):
+                isaftercapture = False
+                move = piece.position
+                previousrank = move // 8
+                while 0 <= move <= 63:
+                    move = move + 7 + 2 * (i % 2) if i < 2 else move - 7 - 2 * (i % 2)
+                    if abs(move // 8 - previousrank) != 1:
+                        break
+                    if move in allpositions[int(piece.isblue)] and not isaftercapture:
+                        break
+                    elif move in allpositions[int(not piece.isblue)]:
+                        if isaftercapture:
+                            break
+                        else:
+                            piececaptured = move
+                            isaftercapture = True
+                    elif isaftercapture and move not in allpositions[0] and move not in allpositions[1]:
+                        self.captures.append([self.allpieces[int(bluetomove)].index(piece), move,
+                                              allpositions[not piece.isblue].index(piececaptured)])
+                    else:
+                        possiblemoves.append(move)
+                    previousrank = move // 8
+        else:
+            possiblemoves = [piece.position - 7 if bluetomove else piece.position + 7,
+                             piece.position - 9 if bluetomove else piece.position + 9]
+            if bluetomove:
+                if possiblemoves[0] // 8 != piece.position // 8 - 1:
+                    possiblemoves.pop(0)
+                if possiblemoves[-1] // 8 != piece.position // 8 - 1:
+                    possiblemoves.pop(-1)
+            else:
+                if possiblemoves[0] // 8 != piece.position // 8 + 1:
+                    possiblemoves.pop(0)
+                if possiblemoves[-1] // 8 != piece.position // 8 + 1:
+                    possiblemoves.pop(-1)
+            for allypiece in self.allpieces[int(bluetomove)]:
+                if allypiece.position in possiblemoves:
+                    possiblemoves.remove(allypiece.position)
+            for enemypiece in self.allpieces[int(not bluetomove)]:
+                if enemypiece.position in possiblemoves:
+                    possiblemoves.remove(enemypiece.position)
+                    positionaftercapture = (enemypiece.position - piece.position) * 2 + piece.position
+                    if positionaftercapture not in allpositions[0] and positionaftercapture not in allpositions[
+                        1] and \
+                            positionaftercapture // 8 == piece.position // 8 + 2 - 4 * int(bluetomove) and \
+                            0 < positionaftercapture // 8 < 8:
+                        # add to capturs list [index of piece, positionaftercapture, index of enemypiece]
+                        self.captures.append([self.allpieces[int(bluetomove)].index(piece), positionaftercapture,
+                                              self.allpieces[int(not bluetomove)].index(enemypiece)])
+
+        self.possiblemoves.append(possiblemoves)
